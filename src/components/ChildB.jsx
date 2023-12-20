11bee7845b3ab65d77dee54b845ddabfaad4886b
Masoud Ramezani <masoudhra@gmail.com>
2023-12-20 13:26:48 +1300

    passing jsx as a props

--- src/components/ChildB.jsx
+++ src/components/ChildB.jsx
@@ -1,10 +1,11 @@
 
 
-function ChildB({number}) {
+function ChildB({number, text}) {
   return (
     <div style={{border: "2px solid black", padding: "10px", margin: "20px"}}>
       <h3>Child B</h3>
       <p>{number}</p>
+      {text}
     </div>
     
   )
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
-import ChildA from "./ChidA"
-import ChildB from "./ChildB"
+import ChildA from "./ChidA";
+import ChildB from "./ChildB";
 
 function Parent() {
-    const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(0);
+
+  const clickHandler = () => {
+    setNumber((number) => number + 1);
+  };
   return (
-    <div style={{border: "2px solid black", padding: "10px", margin: "20px"}}>
-        <h3>Parent</h3>
-        <button onClick={() => setNumber((number) => number + 1)}>+</button>
-        <ChildA number={number} setNumber={setNumber}/>
-        <ChildB number={number}/>
+    <div style={{ border: "2px solid black", padding: "10px", margin: "20px" }}>
+      <h3>Parent</h3>
+      <button onClick={() => setNumber((number) => number + 1)}>+</button>
+      <ChildA number={number} setNumber={setNumber} />
+      <ChildB
+        number={number}
+        text={<button onClick={clickHandler}>+</button>}
+      />
     </div>
-  )
+  );
 }
 
-export default Parent
+export default Parent;
